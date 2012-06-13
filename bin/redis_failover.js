5aaad94e88d71e236b8620bece6f3f9e95843024
Carlos Rodriguez <carlos@s8f.org>
2012-06-12 18:25:15 -0700

    Structure app as binary, separate exports

---
+++ bin/redis_failover.js
@@ -0,0 +1,30 @@
+#!/usr/bin/env node
+
+// Module deps.
+var optimist = require('optimist')
+  , NodeManager = require('../lib/node-manager')
+  ;
+var DEFAULT_ZNODE_PATH = '/redis_failover_nodes'
+  , options = {nodes: []}
+  ;
+
+// Setup optimist.
+var argv = optimist
+    .usage('Usage: $0 [OPTIONS]')
+    .alias('n', 'nodes')
+    .demand('n')
+    .describe('n', 'Comma-separated redis host:port pairs')
+    .alias('z', 'zkservers')
+    .describe('z', 'Comma-separated ZooKeeper host:port pairs')
+    .describe('znodepath', 'Znode path override for storing Redis server list')
+    .describe('max-failures', 'Maximum number of failures before manager marks node as unavailable')
+    .alias('h', 'help')
+    .describe('h', 'Display all options')
+    .argv;
+
+argv.n.split(',').forEach(function (val) {
+  var parts = val.split(':');
+  options.nodes.push({host: parts[0], port: parts[1]});
+});
+
+var manager = new NodeManager(options);
